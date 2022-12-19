97ac4f42fb4b5b56ab391e178c8328d6bcf34b14
calvary35 <ksbrown1830@gmail.com>
2022-12-19 06:00:01 -0500

    The Basic Structure of the Solar System

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+        public static void main(String[] args) {
+            SolarSystem Jinny =new SolarSystem();
+            Jinny.Sun=1;
+            Jinny.Moon=2;
+            Jinny.Planets=3;
+
+        }
+
+}
